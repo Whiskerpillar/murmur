3182e9b9710b7970ddec06d62718a52eca9d7931
Whiskerpillar <Mawwnw@gmail.com>
2024-11-12 13:06:39 -0500

    Create provision.sh

---
+++ provision.sh
@@ -0,0 +1,12 @@
+#! /bin/bash/
+
+# provision System for Murmur
+
+chmod +x ~/murmur/murmur-boot.sh
+chmod +x ~/murmur/provision.sh
+
+#echo Starting provision for GPS
+#sudo rm /etc/default/gpsd
+#sudo rm /lib/systemd/system/gpsd.socket
+#mv ~/murmur/gpsd-config-ow /etc/default/gpsd
+#mv ~/murmur/gpsd-socket-config-ow /lib/systemd/system/gpsd.socket
