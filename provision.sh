c3e5d3aada48940f6f87053fdcf373434f41a6f5
Whiskerpillar <Mawwnw@gmail.com>
2024-11-13 12:49:24 -0500

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,9 +1,24 @@
 #! /bin/bash/
 
-# provision System for Murmur
+echo Provision Raspbian System for Murmur 1.03
 
-chmod +x ~/murmur/murmur-boot.sh
-chmod +x ~/murmur/provision.sh
+echo Installing Script at /usr/local/bin/
+sudo rm /usr/local/bin/murmur-boot.sh
+sudo mv ~/murmur/murmur-boot.sh /usr/local/bin/
+chmod +x /usr/local/bin/murmur-boot.sh
+
+echo Installing Config at /etc/murmur
+sudo mkdir /etc/murmur
+sudo rm /etc/murmur/murmur.conf
+sudo mv ~/murmur/murmur.conf /etc/murmur/
+
+echo We need to add line to rc.local, this is what causes it to boot at start. 
+echo The line is '/usr/local/bin/murmur-boot.sh &' and should be placed a line above the line 'exit 0'
+echo Press any key when ready to open nano to change this. 
+sudo nano /etc/rc.local
+
+
+
 
 #echo Starting provision for GPS
 #sudo rm /etc/default/gpsd
